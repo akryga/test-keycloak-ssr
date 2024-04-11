3f25c099a5f2aa26be8821778eebb394ca613812
akryga <akryga@gmail.com>
2024-04-11 09:31:43 +0300

    provader in standalone component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,62 @@
-import { Component } from '@angular/core';
+import { APP_INITIALIZER, Component, OnInit } from '@angular/core';
+import { NgIf } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { KeycloakService, KeycloakAngularModule } from 'keycloak-angular';
+import { KeycloakProfile } from 'keycloak-js';
+
+function initializeKeycloak(keycloak: KeycloakService) {
+  return () =>
+    keycloak.init({
+      config: {
+        url: 'http://localhost:8080',
+        realm: 'testrealm',
+        clientId: 'myapp'
+      },
+      initOptions: {
+        onLoad: 'check-sso',
+        silentCheckSsoRedirectUri:
+          window.location.origin + '/assets/silent-check-sso.html'
+      }
+    });
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, KeycloakAngularModule, NgIf],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeKeycloak,
+      multi: true,
+      deps: [KeycloakService]
+    }
+  ]
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'test-keycloak-ssr';
+
+  public isLoggedIn: boolean = false;
+  public userProfile?: KeycloakProfile;
+
+  constructor(private readonly keycloak: KeycloakService){}
+  
+  public async ngOnInit() {
+    // this.kc_init();
+    this.isLoggedIn = await this.keycloak.isLoggedIn();
+
+    if (this.isLoggedIn) {
+      this.userProfile = await this.keycloak.loadUserProfile();
+    }
+  }
+
+  public login() {
+    this.keycloak.login();
+  }
+
+  public logout() {
+    this.keycloak.logout();
+  }
 }
